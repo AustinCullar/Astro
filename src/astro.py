981da29337297d400cdcbd98b322e3d956513f11
Austin Cullar <austincullar.work@gmail.com>
2024-09-25 13:47:14 -0600

    Resolve linting errors identified by flake8 (#16)
    * Resolve linting errors identified by flake8

--- src/astro.py
+++ src/astro.py
@@ -5,76 +5,79 @@ leverage the YouTube Data API to gather data from YouTube videos.
 import os
 import argparse
 
-import pandas as pd
-
 from dotenv import load_dotenv
 from data_collection.yt_data_api import YouTubeDataAPI
 from data_collection.sentiment import SentimentAnalysis
 from log import Logger
 from astro_db import AstroDB
 
+
 def extract_video_id_from_url(url: str) -> str:
-	"""
-	Grab the video ID from the provided URL. The ID will come after
-	the substring 'v=' in the URL, so I just split the string on that
-	substring and return the latter half.
-	"""
-	video_id = url.split('v=')[1]
-	return video_id
+    """
+    Grab the video ID from the provided URL. The ID will come after
+    the substring 'v=' in the URL, so I just split the string on that
+    substring and return the latter half.
+    """
+
+    video_id = url.split('v=')[1]
+    return video_id
+
 
 def parse_args():
-	"""
-	Argument parsing logic. Returns the arguments parsed from the CLI
-	"""
-	parser = argparse.ArgumentParser()
+    """
+    Argument parsing logic. Returns the arguments parsed from the CLI
+    """
 
-	parser.add_argument("youtube_url", type=str, help="URL to youtube video")
-	parser.add_argument("-l", "--log", type=str, choices=['debug', 'info', 'warn', 'error'],
-		help='Set the logging level')
+    parser = argparse.ArgumentParser()
 
-	args = parser.parse_args()
+    parser.add_argument("youtube_url", type=str, help="URL to youtube video")
+    parser.add_argument("-l", "--log", type=str, choices=['debug', 'info', 'warn', 'error'],
+                        help='Set the logging level')
 
-	return args
+    args = parser.parse_args()
+
+    return args
 
 
 def main():
-	# parse arguments
-	args = parse_args()
-	video_id = extract_video_id_from_url(args.youtube_url)
+    # parse arguments
+    args = parse_args()
+    video_id = extract_video_id_from_url(args.youtube_url)
 
-	# load environment variables
-	load_dotenv()
-	
-	# prioritize log level provided on CLI, fallback to env variable
-	log_level = args.log if args.log else os.getenv("LOG_LEVEL")
-	api_key = os.getenv("API_KEY")
-	db_file = os.getenv("DB_FILE")
+    # load environment variables
+    load_dotenv()
 
-	# set up logging
-	logger = Logger(log_level)
-	log = logger.get_logger()
+    # prioritize log level provided on CLI, fallback to env variable
+    log_level = args.log if args.log else os.getenv("LOG_LEVEL")
+    api_key = os.getenv("API_KEY")
+    db_file = os.getenv("DB_FILE")
 
-	# pull comments from specified youtube video
-	youtube = YouTubeDataAPI(logger, api_key)
-	comments_df = youtube.get_comments(video_id)
+    # set up logging
+    logger = Logger(log_level)
+    log = logger.get_logger()
 
-	if not comments_df.empty:
-		comments_df['PSentiment'] = ''
-		comments_df['NSentiment'] = ''
+    # pull comments from specified youtube video
+    youtube = YouTubeDataAPI(logger, api_key)
+    comments_df = youtube.get_comments(video_id)
 
-		sa = SentimentAnalysis(logger)
+    if not comments_df.empty:
+        comments_df['PSentiment'] = ''
+        comments_df['NSentiment'] = ''
 
-		for index, row in comments_df.iterrows():
-			sentiment = sa.get_sentiment(row['comment'])
-			comments_df.loc[index, 'PSentiment'] = sentiment[0]
-			comments_df.loc[index, 'NSentiment'] = sentiment[1]
-		
-	# Database logic
-	db = AstroDB(logger, db_file)
-	db.create_database()
-	db.insert_comment_dataframe(video_id, comments_df)
+        sa = SentimentAnalysis(logger)
 
-	log.debug('Collected data preview: \n{}'.format(comments_df))
+        for index, row in comments_df.iterrows():
+            sentiment = sa.get_sentiment(row['comment'])
+            comments_df.loc[index, 'PSentiment'] = sentiment[0]
+            comments_df.loc[index, 'NSentiment'] = sentiment[1]
+
+    # Database logic
+    db = AstroDB(logger, db_file)
+    db.create_database()
+    db.insert_comment_dataframe(video_id, comments_df)
+
+    log.debug('Collected data preview: \n{}'.format(comments_df))
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/data_collection/yt_data_api.py
+++ src/data_collection/yt_data_api.py
@@ -6,76 +6,73 @@ import pandas as pd
 import traceback
 
 from googleapiclient.discovery import build
-from time import sleep
 
-# for debugging purposes
-import json
 
 class YouTubeDataAPI:
-	logger = None
-	api_key = None
 
-	def __init__(self, logger, api_key):
-		self.logger = logger.get_logger()
-		self.api_key = api_key
+    logger = None
+    api_key = None
 
-	def parse_comment_api_response(self, response) -> pd.DataFrame:
-		"""
-		Parse API response for comment query. This will grab all comments and their replies,
-		storing the resulting data in a dataframe.
-		"""
-		df_index = 0
-		df = pd.DataFrame(columns=['comment', 'user', 'date'])
+    def __init__(self, logger, api_key):
+        self.logger = logger.get_logger()
+        self.api_key = api_key
 
-		for item in response['items']:
-			#self.logger.debug('item: {}'.format(item))
-			has_replies = 0 != item['snippet']['totalReplyCount']
-			comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-			user = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
-			date = item['snippet']['topLevelComment']['snippet']['publishedAt']
+    def parse_comment_api_response(self, response) -> pd.DataFrame:
+        """
+        Parse API response for comment query. This will grab all comments and their replies,
+        storing the resulting data in a dataframe.
+        """
+        df_index = 0
+        df = pd.DataFrame(columns=['comment', 'user', 'date'])
 
-			df.loc[df_index] = [comment, user, date]
-			df_index += 1
+        for item in response['items']:
+            has_replies = 0 != item['snippet']['totalReplyCount']
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            user = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
+            date = item['snippet']['topLevelComment']['snippet']['publishedAt']
 
-			if has_replies:
-				for reply in item['replies']['comments']:
-					comment = reply['snippet']['textDisplay']
-					user = reply['snippet']['authorDisplayName']
-					date = reply['snippet']['publishedAt']
+            df.loc[df_index] = [comment, user, date]
+            df_index += 1
 
-					df.loc[df_index] = [comment, user, date]
-					df_index += 1
+            if has_replies:
+                for reply in item['replies']['comments']:
+                    comment = reply['snippet']['textDisplay']
+                    user = reply['snippet']['authorDisplayName']
+                    date = reply['snippet']['publishedAt']
 
-		return df
+                    df.loc[df_index] = [comment, user, date]
+                    df_index += 1
 
-	def get_comments(self, video_id: str) -> pd.DataFrame:
-		"""
-		Collect and store comment information in a dataframe. Collected
-		info includes:
+        return df
 
-		* Username
-		* Comment text
-		* Publish date
+    def get_comments(self, video_id: str) -> pd.DataFrame:
+        """
+        Collect and store comment information in a dataframe. Collected
+        info includes:
 
-		"""
-		youtube = build('youtube', 'v3', developerKey=self.api_key)
+        * Username
+        * Comment text
+        * Publish date
 
-		request = youtube.commentThreads().list(
-			part="snippet,replies",
-			videoId=video_id,
-			textFormat="plainText")
+        """
+        youtube = build('youtube', 'v3', developerKey=self.api_key)
 
-		comment_dataframe = None
+        request = youtube.commentThreads().list(
+            part="snippet,replies",
+            videoId=video_id,
+            textFormat="plainText")
 
-		while request:
-			try:
-				response = request.execute()
-				comment_dataframe = self.parse_comment_api_response(response)
-				break
+        comment_dataframe = None
 
-			except Exception as e:
-				self.logger.error(str(e))
-				self.logger.error(traceback.format_exc())
-				break
+        while request:
+            try:
+                response = request.execute()
+                comment_dataframe = self.parse_comment_api_response(response)
+                break
 
-		return comment_dataframe
+            except Exception as e:
+                self.logger.error(str(e))
+                self.logger.error(traceback.format_exc())
+                break
+
+        return comment_dataframe
