659323201d66052b91a77ffb4c41a24d4b2c7c37
Austin Cullar <austincullar.work@gmail.com>
2024-09-25 13:22:21 -0600

    Resolve linting errors identified by flake8

--- src/data_collection/sentiment.py
+++ src/data_collection/sentiment.py
@@ -10,60 +10,64 @@ import nltk
 from nltk.corpus import wordnet as wn
 from nltk.corpus import sentiwordnet as swn
 
+
 class SentimentAnalysis:
-	def __init__(self, logger):
-		self.logger = logger.get_logger()
 
-	def get_sentiment(self, comment: str) -> ():
-		token_comment = nltk.word_tokenize(comment)
-		pos_tag_comment = nltk.pos_tag(token_comment)
+    def __init__(self, logger):
 
-		positive_sentiment = 0.0
-		negative_sentiment = 0.0
-		objectivity = 1.0
+        self.logger = logger.get_logger()
 
-		for word_tag in pos_tag_comment:
-			word = word_tag[0]
-			tag = word_tag[1]
+    def get_sentiment(self, comment: str) -> ():
 
-			if tag.startswith('J'):
-				tag = wn.ADJ
-			elif tag.startswith('R'):
-				tag = wn.ADV
-			elif tag.startswith('N'):
-				tag = wn.NOUN
-			else:
-				continue
+        token_comment = nltk.word_tokenize(comment)
+        pos_tag_comment = nltk.pos_tag(token_comment)
 
-			# Get synonyms for current word, 'synset'.
-			word_synset = wn.synsets(word, pos=tag)
-			if not word_synset:
-				continue
+        positive_sentiment = 0.0
+        negative_sentiment = 0.0
+        objectivity = 1.0
 
-			# Naive/inexpensive approach: use the first word in the set
-			chosen_synset = word_synset[0]
+        for word_tag in pos_tag_comment:
+            word = word_tag[0]
+            tag = word_tag[1]
 
-			# Retrieve sentiment score for the given synonym from SentiWordNet
-			senti_word_net = swn.senti_synset(chosen_synset.name())
+            if tag.startswith('J'):
+                tag = wn.ADJ
+            elif tag.startswith('R'):
+                tag = wn.ADV
+            elif tag.startswith('N'):
+                tag = wn.NOUN
+            else:
+                continue
 
-			positive_sentiment += senti_word_net.pos_score()
-			negative_sentiment += senti_word_net.neg_score()
-			objectivity = 1 - (positive_sentiment + negative_sentiment)
+            # Get synonyms for current word, 'synset'.
+            word_synset = wn.synsets(word, pos=tag)
+            if not word_synset:
+                continue
 
-			return (positive_sentiment, negative_sentiment, objectivity)
+            # Naive/inexpensive approach: use the first word in the set
+            chosen_synset = word_synset[0]
 
-		return (positive_sentiment, negative_sentiment, objectivity)
+            # Retrieve sentiment score for the given synonym from SentiWordNet
+            senti_word_net = swn.senti_synset(chosen_synset.name())
 
-	def get_sentiment_multiple(self, comments: list) -> list:
-		comments_with_sentiment = []
+            positive_sentiment += senti_word_net.pos_score()
+            negative_sentiment += senti_word_net.neg_score()
+            objectivity = 1 - (positive_sentiment + negative_sentiment)
 
-		for comment in comments:
-			# comment_sentiment: (user, comment, timestamp)
-			comment_sentiment = self.get_sentiment(comment[1])
+            return (positive_sentiment, negative_sentiment, objectivity)
 
-			comment_with_sentiment = comment + comment_sentiment
+        return (positive_sentiment, negative_sentiment, objectivity)
 
-			comments_with_sentiment.append(comment_with_sentiment)
+    def get_sentiment_multiple(self, comments: list) -> list:
 
-		return comments_with_sentiment
-			
+        comments_with_sentiment = []
+
+        for comment in comments:
+            # comment_sentiment: (user, comment, timestamp)
+            comment_sentiment = self.get_sentiment(comment[1])
+
+            comment_with_sentiment = comment + comment_sentiment
+
+            comments_with_sentiment.append(comment_with_sentiment)
+
+        return comments_with_sentiment
