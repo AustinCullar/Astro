8deea62cd03440cbfb8946a8163b38df8e92f62b
Austin Cullar <austincullar.work@gmail.com>
2024-09-25 12:13:36 -0600

    - Add tests for YouTubeDataAPI class - conftest.py was added with basic fixtures - test_comment_api_response.json was added to capture an example API reply 	- This will be parametrized and used in testing

---
+++ src/tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+import json
+import googleapiclient
+
+from unittest.mock import MagicMock
+from src.log import Logger
+
+@pytest.fixture(scope='class')
+def api_comment_response():
+	response = {}
+
+	with open('test_comment_api_response.json', 'r') as f:
+		response = json.load(f)
+
+	return response
+
+@pytest.fixture
+def logger():
+	return Logger('debug')
+
+@pytest.fixture
+def mock_google_http_request(api_comment_response):
+	mock = googleapiclient.http.HttpRequest
+	mock.execute = MagicMock(return_value=api_comment_response)
